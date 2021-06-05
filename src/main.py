5a78148fbf6f6aca8c755961aef4be75c52bbf38
Krzem <krzem5.dev@gmail.com>
2021-06-05 21:53:27 +0200

    Push Update 06/05/2021, 21:53:24

--- src/main.py
+++ src/main.py
@@ -1,29 +1,90 @@
-def solve_xor(a,b):
-	l=len(a)
-	h=0
-	k=0
-	while (h<l and k<l):
-		mx=0
-		mx_v=0
-		for i in range(h,l):
-			if (a[i][k]>mx_v):
-				mx=i
-				mx_v=a[i][k]
+def _solve_xor(a,b):
+	m=len(a)
+	n=len(a[0])
+	i=0
+	for j in range(0,n):
+		mx=i
+		mx_v=a[i][j]
+		while (mx_v==0):
+			mx+=1
+			if (mx==m):
+				break
+			mx_v=a[mx][j]
 		if (mx_v!=0):
-			a[h],a[mx]=a[mx],a[h]
-			b[h],b[mx]=b[mx],b[h]
-			for i in range(h+1,l):
-				for j in range(k+1,l):
-					a[i][j]^=(a[h][j]&a[i][k])
-				b[i]^=a[i][k]&b[k]
-				a[i][k]=0
-			h+=1
-		k+=1
-	for i in range(l-1,-1,-1):
-		for j in range(l-1,i,-1):
-			b[i]^=b[j]&a[i][j]
-	return b
+			for k in range(0,n):
+				t=a[mx][k]
+				a[mx][k]=a[i][k]
+				a[i][k]=t
+			for k in range(i+1,m):
+				for l in range(j+1,n):
+					a[k][l]^=a[i][l]&a[k][j]
+				a[k][j]=0
+			i+=1
+			if (i==m):
+				break
+	o=[0 for _ in range(0,n)]
+	for i in range(0,m):
+		for j in range(0,n):
+			o[j]^=b[i]&a[i][j]
+	return o
 
 
 
-print(solve_xor([[1,1,1],[1,1,0],[0,1,1]],[1,0,1]))
+def solve(b,*l):
+	o=[]
+	v=[]
+	vm=[]
+	vml=0
+	a=[]
+	for k in l:
+		e=[0 for _ in range(0,len(v))]
+		for se in k.split("^"):
+			if (se not in v):
+				while (len(vm)<len(se)):
+					vm.append([])
+					vml+=1
+				vm[len(se)-1].append((se,len(v)))
+				o.append(None)
+				v.append(se)
+				e.append(1)
+			else:
+				e[v.index(se)]=1
+		a.append(e)
+	for k in a:
+		for _ in range(0,len(v)-len(k)):
+			k.append(0)
+	while (True):
+		s=_solve_xor([e[:] for e in a],b[:])
+		if (vml==1):
+			for i in range(0,len(v)):
+				if (o[i] is None):
+					o[i]=s[i]
+			om={}
+			for i,k in enumerate(v):
+				if (len(k)==1):
+					om[k]=o[i]
+			return om
+		for k in vm[vml-1]:
+			if (s[k[1]]==1):
+				i=v.index(k[0])
+				o[i]=1
+				for j in range(0,len(a)):
+					if (a[j][i]==1):
+						a[j][i]=0
+						b[j]^=1
+				for e in k[0]:
+					i=v.index(e)
+					o[i]=1
+					for j in range(0,len(a)):
+						if (a[j][i]==1):
+							a[j][i]=0
+							b[j]^=1
+			else:
+				print("Unknown Combination!")
+		vml-=1
+		while (len(vm[vml-1])==0):
+			vml-=1
+
+
+
+print(solve([0,1],"a^b^c","a^b^ab"))
