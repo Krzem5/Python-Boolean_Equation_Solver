3ab2ab9a5da59c59c89314c44d2a863e49479338
Krzem <krzem5.dev@gmail.com>
2021-06-07 21:43:18 +0200

    Push Update 06/07/2021, 21:43:15

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-def bsf(n):
+def alloc(n):
+	return [None for _ in range(0,n)]
+
+
+
+def __bsf(n):
 	o=0
 	while (not (n&1)):
 		n>>=1
@@ -7,22 +12,21 @@ def bsf(n):
 
 
 
-def bsr(n):
-	o=63
-	m=0x8000000000000000
-	while (not (n&m)):
-		m>>=1
-		o-=1
-	return o
-
-
-
-
-def popcnt(n):
+def __popcnt(n):
 	o=0
 	while (n):
 		if (n&1):
 			o+=1
+		n>>=1
+	return o
+
+
+
+def __parity(n):
+	o=0
+	while (n):
+		if (n&1):
+			o^=1
 		n>>=1
 	return o
 
@@ -40,7 +44,7 @@ def solve(b,*l):
 			if (e not in v):
 				v.append(e)
 				vl+=1
-			i=popcnt(e)
+			i=__popcnt(e)
 			if (i>vl):
 				vl=i
 			while (vml<i):
@@ -51,65 +55,69 @@ def solve(b,*l):
 			mx|=e
 	if (mx&(mx+1)):
 		raise RuntimeError("Not All Identifiers Used")
-	mx_sz=bsr(mx)+1
 	o=0
 	om=mx
-	a=[0 for _ in range(0,len(b))]
+	sz=len(l)
+	a=alloc(sz)
+	na=alloc(sz)
 	for i,k in enumerate(l):
+		a[i]=0
 		for e in k:
 			a[i]|=1<<v.index(e)
+		na[i]=a[i]
 	while (True):
-		na=a[:]
 		i=0
+		m=1
 		for j in range(0,vl):
-			mx=i
-			mx_v=na[i]&(1<<j)
-			while (mx_v==0):
-				mx+=1
-				if (mx==len(a)):
+			k=i
+			t=na[i]
+			while (not (t&m)):
+				k+=1
+				if (k==len(a)):
 					break
-				mx_v=na[mx]&(1<<j)
-			if (mx_v!=0):
-				t=na[mx]
-				na[mx]=na[i]
+				t=na[k]
+			if (t&m):
+				na[k]=na[i]
 				na[i]=t
-				for k in range(i+1,len(a)):
-					if (na[k]&(1<<j)):
-						na[k]=(na[k]&(~(1<<j)))^na[i]
+				for k in range(i+1,sz):
+					if (na[k]&m):
+						na[k]^=na[i]^m
 				i+=1
 				if (i==len(a)):
 					break
+			m<<=1
 		s=0
-		for i in range(0,len(a)):
-			for j in range(0,vl):
-				s^=(b[i]&(na[i]>>j))<<j
+		m=b
+		while (m):
+			s^=na[__bsf(m)]
+			m&=m-1
 		if (vml==1):
 			return o|(s&om)
-		vml-=1
-		for k in vm[vml]:
+		rm=True
+		for k in vm[vml-1]:
 			m=1<<v.index(k)
 			if (s&m):
-				for i in range(0,len(a)):
-					if (a[i]&m):
-						a[i]&=~m
-						b[i]^=1
+				om&=~k
+				o|=k
 				while (k):
-					i=1<<bsf(k)
-					k&=~i
-					om&=~i
-					o|=i
-					m=1<<v.index(i)
-					for j in range(0,len(a)):
-						if (a[j]&m):
-							a[j]&=~m
-							b[j]^=1
+					m|=1<<v.index(k&(-k))
+					k&=k-1
+				im=~m
+				for i in range(0,sz):
+					b^=__parity(a[i]&m)<<i
+					a[i]&=im
 			else:
+				rm=False
 				print("Unknown Combination")
-		while (len(vm[vml])==0):
+		if (rm):
 			vml-=1
+			while (len(vm[vml])==0):
+				vml-=1
+		for i in range(0,sz):
+			na[i]=a[i]
 
 
 
-o=solve([1,0],[1,2,3],[1,2,4])
+o=solve(0b01,[0b001,0b010,0b011],[0b001,0b010,0b100])
 print(f"a^b^(a&b)=1, a^b^c=0 -> a={o&1}, b={(o>>1)&1}, c={o>>2}")
 
